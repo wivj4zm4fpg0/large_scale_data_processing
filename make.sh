bef007a60d88e69a544d927698a8864186673018
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2018-09-08 05:33:37 +0900

    correspond c++17.

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 
-g++ $1 `pkg-config --cflags opencv` `pkg-config --libs opencv` -o $(echo $1 | sed -e 's/\.cpp//g')
+g++-7 $1 `pkg-config --cflags opencv` `pkg-config --libs opencv` -o $(echo $1 | sed -e 's/\.cpp//g') -lstdc++fs
 
